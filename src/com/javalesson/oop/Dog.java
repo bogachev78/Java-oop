1fb14b055047bde4dd11c15d973aa0791bcee4fc
Andrey <bogachev78@gmail.com>
2021-02-02 15:44:25 +0300

    1.03

--- src/com/javalesson/oop/Dog.java
+++ src/com/javalesson/oop/Dog.java
@@ -4,11 +4,11 @@ public class Dog {
 
     private static int dogCount;
 
-    private int paws;
-    private int tail;
+    public static final int PAWS = 4;
+    public static final int TAIL = 1;
     private String name;
     private String breed;
-    private String size;
+    private Size size = Size.UNDEFINED;
 
     public Dog(){
         dogCount++;
@@ -19,19 +19,12 @@ public class Dog {
         return dogCount;
     }
 
-    public String getSize() {
+    public Size getSize() {
         return size;
     }
 
-    public void setSize(String size) {
-        if(size.equalsIgnoreCase("Big")||
-                size.equalsIgnoreCase("Average")||
-                size.equalsIgnoreCase("Small")){
-            this.size = size;
-        }else{
-            System.out.println("Size should be one of these: Big, Average or Small");
-        }
-
+    public void setSize(Size size) {
+        this.size = size;
     }
 
     public void setName(String name) {
@@ -40,35 +33,6 @@ public class Dog {
 
     public String getName() {
         return name;
-    }
-
-    public void setPaws(int paws) {
-        if (paws == 4) {
-            this.paws = paws;
-        } else {
-            this.paws = 4;
-            System.out.println("User tried to assign " + paws + "  paws  for a dog.");
-            System.out.println("Correct number is 4");
-        }
-
-    }
-
-    public int getPaws() {
-        return paws;
-    }
-
-    public int getTail() {
-        return tail;
-    }
-
-    public void setTail(int tail) {
-        if (tail == 1) {
-            this.tail = tail;
-        } else {
-            this.tail = 1;
-            System.out.println("User tried to assign " + tail + " tail  for a dog.");
-            System.out.println("Correct number is 1");
-        }
     }
 
     public String getBreed() {
@@ -80,12 +44,18 @@ public class Dog {
     }
 
     public void bark(){
-        if(size.equalsIgnoreCase("Big")){
-            System.out.println("Wof - Wof");
-        }else if(size.equalsIgnoreCase("Average")){
-            System.out.println("Raf - Raf");
-        }else{
-            System.out.println("Tiaf - Tiaf");
+        switch (size){
+            case BIG:case VERY_BIG:
+                System.out.println("Wof-Wof");
+                break;
+            case AVERAGE:
+                System.out.println("Raf-Raf");
+                break;
+            case SMALL:case VERY_SMALL:
+                System.out.println("Tiaf-Tiaf");
+                break;
+            default:
+                System.out.println("Dogs's size undefined");
         }
     }
 
--- src/com/javalesson/oop/Main.java
+++ src/com/javalesson/oop/Main.java
@@ -6,30 +6,21 @@ public class Main {
         Dog.getDogCount();
 
         Dog lab = new Dog();
-        lab.setPaws(4);
-        lab.setTail(1);
         lab.setName("Charley");
         lab.setBreed("Lab");
-        lab.setSize("Average");
+        lab.setSize(Size.AVERAGE);
         lab.bite();
 
         Dog shepard = new Dog();
-        shepard.setPaws(4);
-        shepard.setTail(1);
         shepard.setName("Mike");
         shepard.setBreed("Shepard");
-        shepard.setSize("Big");
+        shepard.setSize(Size.BIG);
         shepard.bite();
 
         Dog doberman = new Dog();
-        doberman.setPaws(4);
-        shepard.setTail(1);
         shepard.setName("Jack");
         shepard.setBreed("Doberman");
-        shepard.setSize("Big");
+        shepard.setSize(Size.BIG);
         shepard.bite();
-
-        System.out.println("Lab's name is " + lab.getName());
-        System.out.println("Shepard's name is " + shepard.getName());
     }
 }
